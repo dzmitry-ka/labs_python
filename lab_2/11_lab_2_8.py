a7bbacd627ec832b367ccf80f18fbcfbd9d93f79
Dzmitry K <61231055+MrDmitryBy@users.noreply.github.com>
2020-03-29 00:34:23 +0300

    Ready
    This programm is available only for integer numbers, because if we enter not an integer number, but a float one (for example, 0.25 or 1/4), the program will throw an error, since the logical "and" does not support the float type.
    P.S. The fastest calculating way is to check them for bits, but then the program does not affect the float, and if we do it through the logarithm, then it will take much more time in time than with bits.

---
+++ lab_2/11_lab_2_8.py
@@ -0,0 +1,38 @@
+#Power of 2
+import sys
+
+def main():
+     while True:
+        try: 
+            if len(sys.argv[0]) > 1:
+                x = sys.argv[1]
+        except:        
+            x = input("""\tEnter the number: 
+                
+                
+                      !ATTENTION! 
+                      If you want exit from the programm, enter "exit" 
+                      """)
+        if x == "exit":
+            break
+        else: 
+            try:
+                x = int(x)
+                def power(x):
+                    if x & (x - 1) == 0:
+                        return 1
+                if power(x) == 1:
+                    print()
+                    print("Yes, number ", x, " is a power of 2")                    
+                else: 
+                    print()
+                    print("Unfortunately, number ", x, " isn't a power of 2")
+                
+            except:
+                print()
+                print("You make a mistake, please, write \n \
+                      an integer positive number: ")
+     
+if __name__ == "__main__":
+ main()
+ 
