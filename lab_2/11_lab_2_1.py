54e64e7685a0163276231dc7fed4699d7c2eadcf
Dzmitry K <61231055+MrDmitryBy@users.noreply.github.com>
2020-03-30 01:01:59 +0300

    In process

--- lab_2/11_lab_2_1.py
+++ lab_2/11_lab_2_1.py
@@ -1,1 +1,74 @@
+import math
+def main():
+    while True:
+        array = []
+        array = input('Enter the elements of array'
+                      'If you want to exit, please, enter "exit" ')
+        if array == "exit":
+            print("Good bye!")
+            break
+        else:
+            while True:
+                if array == "stop" or "STOP":
+                    return False
+                else:
+                    try:
+                        array = array.append(float(array))
+                        sizeArray = len(array)
+                        sizeBlocks = math.ceil(math.sqrt(sizeArray))
+                def blocks():
+                    block = [0]*sizeBlocks
+                    i = 0
+                    for i in sizeArray - 1:
+                        block[i//sizeBlocks] = array[i]
+                        i += 1
+                        return block
+                    def findSum():
+                        while True:
+                            sum = 0
+                            left = input('From what part of an array'
+                                         'should I start?'
+                                         'If you want to exit, please,' 
+                                         'enter "exit" ') 
+                            if left == "exit":
+                                print("Good bye!")
+                                break
+                            else:
+                                try:
+                                    left = int(left)
+                                    right = input('By what part of an array'
+                                            'should I finish?'
+                                            'If you want to exit, please,' 
+                                            'enter "exit" ')
+                                    if right == "exit":
+                                        print("Good bye!")
+                                        break
+                                    else:
+                                        try:
+                                            right = int(right)
+                                            try:
+                                                if left >= 0 and left <= right: 
+                                                    i = left
+                                                    while (i <= right):
+                                                        sum += array[i]
+                                                        i +=1
+                                                    return sum
+                                                print("Sum is equal to ", sum)
+                                    
+                                            except: print("You make a mistake! Please, " 
+                                                          "write start number >= 0 and finish number greater than start")
+                                                    print('If you want to exit, please,enter "exit"')
+                                        except: 
+                                            print("You make a mistake! Please, " 
+                                                  "write an integer number")
+                                            print('If you want to exit, please,enter "exit"')
+                                except:
+                                    print("You make a mistake! Please, " 
+                                          "write an integer number")
+                                    print('If you want to exit, please,enter "exit"')
+                                        
+                    except: 
+                        print("You make a mistake! Please, write a float number(-s)")
 
+if __name__ == "__main__":
+    main()
