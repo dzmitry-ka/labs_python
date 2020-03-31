f6176929196de96f2cac34503c39b3e9a97766ae
Dzmitry K <61231055+MrDmitryBy@users.noreply.github.com>
2020-03-31 20:13:33 +0300

    Update 11_lab_2_8.py

--- lab_2/11_lab_2_8.py
+++ lab_2/11_lab_2_8.py
@@ -2,36 +2,35 @@
 import sys
 
 def main():
-     while True:
-        try: 
-            if len(sys.argv[0]) > 1:
-                x = sys.argv[1]
-        except:        
+    try: 
+        if len(sys.argv[0]) > 1:
+            x = sys.argv[1]
+    except:
+        while True: 
             x = input("""\tEnter the number: 
-                
-                
-                      !ATTENTION! 
-                      If you want exit from the programm, enter "exit" 
-                      """)
-        if x == "exit":
-            break
-        else: 
-            try:
-                x = int(x)
-                def power(x):
-                    if x & (x - 1) == 0:
-                        return 1
-                if power(x) == 1:
+                    
+                        !ATTENTION! 
+                        If you want exit from the programm, enter "exit" 
+                          """)
+            if x == "exit":
+                break
+            else: 
+                try:
+                    x = int(x)
+                    def power(x):
+                        if x & (x - 1) == 0 and x != 0:
+                            return 1
+                    if power(x) == 1:
+                        print()
+                        print("Yes, number ", x, " is a power of 2")                    
+                    else: 
+                        print()
+                        print("Unfortunately, number ", x, " isn't a power of 2")
+                    
+                except:
                     print()
-                    print("Yes, number ", x, " is a power of 2")                    
-                else: 
-                    print()
-                    print("Unfortunately, number ", x, " isn't a power of 2")
-                
-            except:
-                print()
-                print("You make a mistake, please, write \n \
-                      an integer positive number: ")
+                    print("You make a mistake, please, write \n \
+                          an integer positive number: ")
      
 if __name__ == "__main__":
  main()
