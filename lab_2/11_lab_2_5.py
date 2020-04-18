891a9796f34474f50d26dbc9257ab8a790f8964b
Dzmitry K <61231055+MrDmitryBy@users.noreply.github.com>
2020-04-18 21:28:08 +0300

    Not Ready

--- lab_2/11_lab_2_5.py
+++ lab_2/11_lab_2_5.py
@@ -1,6 +1,6 @@
 import sys
 from ast import literal_eval
-
+from re import search
 
 def to_json(obj):
     try:
@@ -12,16 +12,20 @@ def to_json(obj):
             if type(obj) == None:
                 obj = 'null'
             if type(obj) == bool:
-                obj = "obj"
+                if obj == True:
+                    obj = 'true'
+                else:
+                    obj = 'false'              
             if type(obj) == dict:
                 line = ''
                 for element in range(len(obj)):
-                    line += + ': ' + to_json(obj[element])
-                    if element < len(obj) - 1:
-                        line += ', '
-                    else: break
+                    for item in obj.items():
+                        line = ('"' + to_json(item[0]) + '"' 
+                                +' : ' + to_json(item[1]))
+                        if element < len(obj) - 1:
+                            line += ', '
+                        else: break
                 obj = '{' + line + '}'
-                
             if type(obj) == list or type(obj) == tuple:
                 line = ''
                 for element in range(len(obj)):
@@ -30,7 +34,7 @@ def to_json(obj):
                         line += ', '
                     else: break
                 obj = '[' + line + ']'
-                
+        return obj
                         
                 
                 
@@ -40,25 +44,33 @@ def to_json(obj):
         
 def main():
     if len(sys.argv) > 1:
-        obj = literal_eval(sys.argv[1])
+        try:
+            fileName = sys.argv[1]
+            if search(r'\b.txt\b', fileName):
+                file = open(fileName, "r")
+                obj = file.read()
+                file.close()  
+            else:
+                obj = literal_eval(sys.argv[1])
+                
+        except:    
+            print("You are not enter .txt file or you enter a space in the",
+              "line command.")
+            print("Please, write file without space and with .txt format ")
+            sys.exit()
     else:
         obj = input("Enter your words: ")
     try:
         obj = literal_eval(obj)
-        print(to_json(obj))
+        a = to_json(obj)
+        print(a)
+        print(type(a))
+        
     except ValueError: 
-        print('Array that refers to itself')
+        print("Your object in question is not a type from this list: ")
+        print("float, int, str, tuple, dict, list, None, bool")
+        
         
 if __name__ == "__main__":
     main()
-""" MLB_team = {
-    'Colorado' : 'Rockies',
-    'Boston'   : 'Red Sox',
-    'Minnesota': 'Twins',
-    'Milwaukee': 'Brewers',
-    'Seattle'  : 'Mariners'
-    }
 
-for item in MLB_team.items():
-    print(item[0], item[1])
-    print(type(item))"""
